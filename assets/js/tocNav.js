18e3ed145ff610abbb8d4ee492826f7d945b9b2f
Justin Tan How Yuan <justanhowyuan@gmail.com>
2021-05-08 16:15:45 +0100

    update

--- assets/js/tocNav.js
+++ assets/js/tocNav.js
@@ -1,16 +1,5 @@
 function toggleNav() {
-    document.querySelector(".sideNav").classList.toggle("closedNav")
-    // const grid = document.getElementById("mainGrid");
-    // const nav = document.getElementById("sidenav");
-    // if (x.classList.contains('closed')) {
-    //     // grid.style.gridTemplateColumns = "25% 75%";
-    //     nav.style.width = "100%";
-    //     nav.style.overflow = "scroll";
-    // } else {
-    //     // grid.style.gridTemplateColumns = "0% 100%";
-    //     nav.style.width = "0%";
-    //     nav.style.overflow = "hidden";
-    // }
+    document.querySelector(".sideNav").classList.toggle("closedNav");
 }
 
 
