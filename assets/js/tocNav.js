fd99442edef8ce3a924646e1abc1e1cf2e269a9c
Justin Tan How Yuan <justanhowyuan@gmail.com>
2021-05-08 14:22:30 +0100

    Table of contents button

--- assets/js/tocNav.js
+++ assets/js/tocNav.js
@@ -1,7 +1,16 @@
-function openNav() {
-    document.getElementById("sidenav").style.width = "250px";
+function toggleNav(x) {
+    x.classList.toggle("change");
+    const grid = document.getElementById("mainGrid");
+    const nav = document.getElementById("sidenav");
+    if (x.classList.contains('change')) {
+        grid.style.gridTemplateColumns = "25% 75%";
+        nav.style.width = "100%";
+        nav.style.overflow = "scroll";
+    } else {
+        grid.style.gridTemplateColumns = "0% 100%";
+        nav.style.width = "0%";
+        nav.style.overflow = "hidden";
+    }
 }
 
-function closeNav() {
-    document.getElementById("sidenav").style.width = "0";
-}
+
